88d5432c4ed9fea0030a1d22cf3ef717edab6350
Dopesmoker <Siloda24@gmail.com>
2019-05-27 19:53:26 +0300

    rebuild fitst task

--- ITEAhome/Program.cs
+++ ITEAhome/Program.cs
@@ -6,159 +6,164 @@ namespace ITEAhome
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Helo! Enter you total price:");
-            int totalPriсe = Convert.ToInt32(Console.ReadLine());
-            
-            switch (totalPriсe)
-            { 
-                                              
-                case 500:
-                    {
-                        Console.WriteLine("you discount: 7% " + " Total Pay: " + (totalPriсe * 0.93 + "$"));
-                        break;
-                    }
-                case 400:
-                    {
-                        Console.WriteLine("you discount: 5% " + " Total Pay: " + (totalPriсe * 0.95 + "$"));
-                        break;
-                    }
-                case 300:
-                    {
-                        Console.WriteLine("you discount: 3% " + " Total Pay: " + (totalPriсe * 0.93 + "$"));
-                        break;
-                    }
-                case 100:
-                    {
-                        Console.WriteLine("you discount: 0% " + " Total Pay: " + totalPriсe);
-                        
-                        break;
-                    }
-            }
-               
-               int count = 5;
 
-             while (true)
+            Console.WriteLine("Helo! Enter you total price:");
+
+            float i = Convert.ToInt32(Console.ReadLine());
+
+            switch (i)
             {
-                Random rnd = new Random();
-                int value = rnd.Next(0, 100);
-                Console.WriteLine(" ");
-                Console.WriteLine("Guess and enter number, from 0 to 100");
-                int num = Convert.ToInt32(Console.ReadLine());
-
-                if (num < value)
-                {
-                    count--;
-                    Console.WriteLine("Wrong!!! you have: " + count + " assmits.");
-                }
-                if (num > value)
-                {
-                    count--;
-                    Console.WriteLine("Wrong!!! you have: " + count + " assmits.");
-                }
-
-                if (num == value)
-                {
-                    Console.WriteLine("Bingo!!!");
-                    break;
-                }
-
-                if (count==0)
-                {
+                case float n when (n >= 500):
+                    {
+                        Console.WriteLine("you discount 7% " + " Total pay: " + n * 0.93);
+                        break;
+                    }
+                case float n when (n < 500 && n >= 400):
+                    {
+                        Console.WriteLine("you discount 5% " + " Total pay: " + n * 0.95);
+                        break;
+                    }
+                case float n when (n < 400 && n >= 300):
+                    {
+                        Console.WriteLine("you discount 3% " + " Total pay: " + n * 0.97);
+                        break;
+                    }
+                case float n when (n < 300 ):
+                    { 
+                        Console.WriteLine("you discount 0%" + " Total pay: " + n);
                     
-                    Console.WriteLine("Goodbye");
-                    break;
+                        break;
+                    }
 
-                }
+                    
+
+
+                   /* int count = 5;
+                     Random rnd = new Random();
+                     int value = rnd.Next(0, 101);
+                     Console.WriteLine(" ");
+                     Console.WriteLine("Guess and enter number, from 0 to 100");
+
+                     while (true)
+                     {
+
+                         int num = Convert.ToInt32(Console.ReadLine());
+
+                         if (num < value)
+                         {
+                             count--;
+                             Console.WriteLine("Wrong!!! My number is Bigger! You have: " + count + " assmits." );
+                         }
+                         if (num > value)
+                         {
+                             count--;
+                             Console.WriteLine("Wrong!!! My number is Less! You have: " + count + " assmits.");
+                         }
+
+                         if (num == value)
+                         {
+                             Console.WriteLine("Bingo!!! YOU WIN");
+                             break;
+                         }
+
+                         if (count==0)
+                         {
+
+                             Console.WriteLine("YOU LUSSE!!! Goodbye");
+                             break;
+                         }
 
 
 
+                     }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
-} 
+}
